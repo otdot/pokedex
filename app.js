0f27b3be38975e5f8e76ffb9c2536e571ef54f81
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-03-10 14:28:18 +0200

    added pictures

--- app.js
+++ app.js
@@ -4,23 +4,42 @@ const fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon/?limit=50&offset=0/")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.results);
-      data.results.map((pokemon, i) => createCard(pokemon.name));
+      const fetchAll = data.results.map((pokemon) =>
+        fetch(pokemon.url).then((res) => res.json())
+      );
+      Promise.all(fetchAll).then((data) =>
+        data.forEach((item) => {
+          createCard(
+            item.name,
+            item.sprites.other.dream_world.front_default,
+            item.types[0].type.name
+          );
+          console.log(item.sprites.other.dream_world.front_default);
+        })
+      );
     });
 };
 
-const createCard = (data) => {
+// item.sprites.other.dream_world.front_default, item.name item.types[0].type
+const createCard = (data, img, p) => {
   const div = document.createElement("div");
   div.className = "card";
+  const paragraph = document.createElement("p");
   const h1 = document.createElement("h1");
-  const content = document.createTextNode(`${data}`);
-  h1.appendChild(content);
+  const image = document.createElement("img");
+  image.src = img;
+  const h1content = document.createTextNode(`${data}`);
+  const pcontent = document.createTextNode(`${p}`);
+  h1.appendChild(h1content);
+  paragraph.appendChild(pcontent);
+  div.appendChild(image);
   div.appendChild(h1);
+  div.appendChild(paragraph);
+
   cards.appendChild(div);
 };
-
 fetchData();
 
-// fetch("https://pokeapi.co/api/v2/pokemon/1/")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
+fetch("https://pokeapi.co/api/v2/pokemon/1/")
+  .then((res) => res.json())
+  .then((data) => console.log(data.sprites.other.dream_world.front_default));
